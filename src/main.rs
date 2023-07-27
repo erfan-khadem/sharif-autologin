29c8af8d64899b9936f01fa3272b7b54468bfa8e
Erfan Khadem <erfankhademerkh@gmail.com>
2023-07-27 20:34:53 +0330

    Run the logging in procedure periodically

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::{path::Path};
-use std::io;
+use std::{io, thread, path::Path};
 
 use serde::{Serialize, Deserialize};
 use reqwest;
@@ -64,5 +63,16 @@ async fn main() {
         .form(&creds)
         .send()
         .await;
+    tokio::spawn(async move {
+        let client = reqwest::Client::builder().build().unwrap();
+        let creds = get_creds(None).await.unwrap();
+        loop {
+            thread::sleep(Duration::from_secs(3600 * 3)); // Run every 3 hours
+            let _ = client.post("https://net2.sharif.edu/login")
+                .form(&creds)
+                .send()
+                .await;
+        }
+    });
     run(&creds).await.unwrap();
 }
